02a35bf639bf7de7bc8f92891ebdbd72cdb9a39f
Leo <40208520+LeopoldFrilot@users.noreply.github.com>
2021-03-07 19:03:43 -0600

    Starting the HUB setup

---
+++ Assets/Scripts/SceneManagerSimplified.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerSimplified : MonoBehaviour
+{
+    public void LoadCertainSceneByIndex(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+    public void LoadFirstScene()
+    {
+        LoadCertainSceneByIndex(0);
+    }
+
+    public void LoadNextScene(int numScenes = 1 /* Allows you to skip over more than one scene, can be negative */)
+    {
+        int curScene = SceneManager.GetActiveScene().buildIndex;
+        int destinationScene = curScene + numScenes;
+
+        if(destinationScene < 0 || destinationScene > SceneManager.sceneCount - 1)
+        {
+            Debug.LogError("ERROR: Called scene that doesn't exist");
+            return;
+        }
+
+        LoadCertainSceneByIndex(destinationScene);
+    }
+}
